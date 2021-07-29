2d9ff0d0d06fe1944045adc879c6c32ecb51153f
Julien Dhouti <julien@godaddy.com>
2021-07-28 23:25:01 -0700

    Fixed weird linting errors?

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # levels-slack-scrape
 TC updates whenever you want
 
-# how to use
+## How to use
 1. Make sure you have chromedriver in your PATH (depends on OS)
 2. `python -m venv .env`
 3. activate the virtual environment (depends on OS)
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from selenium import webdriver
 import json
+
 import constants
+from selenium import webdriver
+
 
 def main():
     with open(constants.JSON_PATH) as f:
         fyi_links = json.load(f)
     driver = webdriver.Chrome()
     driver.get(fyi_links[constants.NETFLIX_SALARY_LINK])
-    recent_salary = driver.find_element_by_xpath(fyi_links[constants.RECENT_SALARY_XPATH])
+    recent_salary = driver.find_element_by_xpath(
+        fyi_links[constants.RECENT_SALARY_XPATH]
+    )
     recent_yoe = driver.find_element_by_xpath(fyi_links[constants.RECENT_YOE_XPATH])
-    recent_level_name = driver.find_element_by_xpath(fyi_links[constants.RECENT_LEVEL_NAME_XPATH])
+    recent_level_name = driver.find_element_by_xpath(
+        fyi_links[constants.RECENT_LEVEL_NAME_XPATH]
+    )
 
     print(recent_level_name.text, recent_salary.text, recent_yoe.text)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
